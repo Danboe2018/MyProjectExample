436938f725ad5c07e5f6a1f668dbdb5af6842c53
Dan Boehmke <Daniel@webappclouds.com>
2019-11-06 14:53:44 -0500

    Initial Commit

---
+++ src/screens/Camera.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button, View, Text } from 'react-native';
+class Camera extends React.Component {
+    static navigationOptions = {
+        title: 'Camera'
+    };
+    render() {
+        return (
+            <View style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}>
+                <Button title="Go to Home screen"
+                    onPress={() => this.props.navigation.navigate('Home')}
+                />
+            </View>
+        );
+    }
+}
+export default Camera;
